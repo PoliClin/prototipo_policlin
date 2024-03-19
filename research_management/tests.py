2f0ce54c4036d43fbdd9304f644827287df849af
pedro <pedrohirschle@outlook.com>
2024-03-19 17:44:15 -0300

    unit_tests para pesquisas com horários marcados | finalizado unit_tests

--- research_management/tests.py
+++ research_management/tests.py
@@ -2,8 +2,10 @@ from datetime import date, timedelta
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rooms.models import Room
+from system_constants.models import Schedule, Weekdays
 
-from .models import Research
+from research_management.models import Research, ScheduledTimes
 
 User = get_user_model()
 
@@ -53,3 +55,47 @@ class ResearchModelTests(TestCase):
         """Valida se a criação de um pesquisador impossível não foi bem sucedida."""
         self.assertFalse(self.research.researchers.filter(username='researcher_999').exists())
 
+
+class ScheduledTimesModelTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        """Mocks principais para os testes da classe."""
+        cls.user = User.objects.create_user(username='testuser', password='12345')
+
+        cls.research = Research.objects.create(
+            name="Estudo sobre Teste",
+            main_researcher=cls.user,
+            expected_number_of_patients=10,
+        )
+
+        cls.room = Room.objects.create(
+            number=101,
+            exam_collection_room=True,
+        )
+
+        cls.start_schedule = Schedule.objects.create(time="09:00")
+        cls.end_schedule = Schedule.objects.create(time="17:00")
+        cls.monday = Weekdays.objects.create(name="Segunda-feira")
+        cls.tuesday = Weekdays.objects.create(name="Terça-feira")
+
+        cls.scheduled_time = ScheduledTimes.objects.create(
+            start_schedule=cls.start_schedule,
+            end_schedule=cls.end_schedule,
+            research=cls.research,
+            room=cls.room,
+        )
+        cls.scheduled_time.weekday.add(cls.monday, cls.tuesday)
+
+    def test_scheduled_time_creation(self):
+        """Valida a criação correta do objeto de Pesquisa com horário marcado."""
+        self.assertEqual(str(self.scheduled_time), f"{self.research} - {self.room}")
+        self.assertTrue(self.scheduled_time.weekday.filter(name="Segunda-feira").exists())
+        self.assertTrue(self.scheduled_time.weekday.filter(name="Terça-feira").exists())
+        self.assertEqual(self.scheduled_time.start_schedule, self.start_schedule)
+        self.assertEqual(self.scheduled_time.end_schedule, self.end_schedule)
+
+
+    def test_invalid_weekday_scheduled_time_creation(self):
+        """Valida objeto inexistente com dia de semana inválido."""
+        self.assertFalse(self.scheduled_time.weekday.filter(name="Sábado").exists())
