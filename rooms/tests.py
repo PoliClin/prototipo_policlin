d6716a400c27c22c7c1783ad28d3543ebab17695
Carlos Eduardo <carlos.devvv@gmail.com>
2024-03-19 22:46:23 -0300

    Update tests.py

--- rooms/tests.py
+++ rooms/tests.py
@@ -1,6 +1,7 @@
-# Create your tests here.
 from django.test import TestCase
-
+from django.contrib.admin.sites import AdminSite
+from .apps import RoomsConfig
+from .admin import RoomAdmin
 from .models import Room
 
 
@@ -33,3 +34,54 @@ class RoomModelTest(TestCase):
             Room.objects.create(number=None)
         with self.assertRaises(Exception):
             Room.objects.create(number=103, exam_collection_room=None, archive_room=None)
+
+class RoomAdminTest(TestCase):
+    def setUp(self):
+        self.admin_site = AdminSite()
+        self.room_admin = RoomAdmin(Room, self.admin_site)
+
+    def test_list_display(self):
+        """
+        Verifica se os campos especificados em list_display estão corretos.
+        """
+        self.assertEqual(self.room_admin.list_display, ('_number', 'exam_collection_room', 'archive_room'))
+
+    def test_search_fields(self):
+        """
+        Verifica se os campos especificados em search_fields estão corretos.
+        """
+        self.assertEqual(self.room_admin.search_fields, ('number',))
+
+    def test_list_filter(self):
+        """
+        Verifica se os campos especificados em list_filter estão corretos.
+        """
+        self.assertEqual(self.room_admin.list_filter, ('archive_room', 'exam_collection_room'))
+
+    def test_number_display(self):
+        """
+        Verifica se o método _number exibe o número da sala corretamente.
+        """
+        # Criando um objeto Room para uso no teste
+        room = Room(number='101')
+        # Testando o método _number
+        self.assertEqual(self.room_admin._number(room), 'Sala 101')
+
+class RoomsConfigTest(TestCase):
+    def test_default_auto_field(self):
+        """
+        Verifica se o default_auto_field é definido corretamente.
+        """
+        self.assertEqual(RoomsConfig.default_auto_field, 'django.db.models.BigAutoField')
+
+    def test_name(self):
+        """
+        Verifica se o nome da aplicação está correto.
+        """
+        self.assertEqual(RoomsConfig.name, 'rooms')
+
+    def test_verbose_name(self):
+        """
+        Verifica se o verbose_name está definido corretamente.
+        """
+        self.assertEqual(RoomsConfig.verbose_name, 'Gerenciamento de Salas')
